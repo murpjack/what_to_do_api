f38507e3fa341c0bfed37d186d883cc3cea7b459
Jack <jackmurphy756@gmail.com>
2022-05-11 06:21:18 +0100

    WIP: Hello world to verify server connection

--- src/index.ts
+++ src/index.ts
@@ -8,5 +8,11 @@ const port = process.env.PORT || 1234;
 // DATABASE
 // db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
+app.get('/', (req, res) => {
+  res.send('Halo Welt!');
+});
+
 app.use('/api', router);
-app.listen(port, () => console.log(`Server on http://localhost:${port}/`));
+app.listen(port, () =>
+  console.log(`Server on http://localhost:${port}/`),
+);
