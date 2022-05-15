50b983bca55c0ac240554481fbe0d1d0c34653c2
Jack <jackmurphy756@gmail.com>
2022-05-15 07:14:49 +0100

    WIP: create dummy route callbacks

--- src/controllers/hospitality.ts
+++ src/controllers/hospitality.ts
@@ -17,8 +17,39 @@ export const getAllVenues = async (_: any, res: any) => {
     .json({ success: true, data: [{ a: 'boo', b: 'bah' }] });
 };
 
+export const createVenue = async (_: any, res: any) => {
+  return res
+    .status(201)
+    .json({ success: true, data: 'Venue created' });
+};
+
+export const updateVenue = async (_: any, res: any) => {
+  return res
+    .status(200)
+    .json({ success: true, data: 'venue updated' });
+};
+
+export const requestDeleteVenue = async (_: any, res: any) => {
+  return res
+    .status(202)
+    .json({
+      success: true,
+      data: 'Request submitted. Pending approval',
+    });
+};
+
+export const deleteVenue = async (_: any, res: any) => {
+  // TODO: Should this have a code 410 or 200?
+  // 410 = resource gone; 200 = success
+  return res
+    .status(410)
+    .json({ success: true, data: 'venue deleted' });
+};
+
 export default {
   ['get::getAllVenues']: getAllVenues,
-  ['post::postAllVenues']: () => {},
-  ['delete::deleteAllVenues']: () => {},
+  ['post::createVenue']: createVenue,
+  ['put::updateVenue']: updateVenue,
+  ['get::requestDeleteVenue']: requestDeleteVenue,
+  ['delete::deleteVenue']: deleteVenue,
 };
